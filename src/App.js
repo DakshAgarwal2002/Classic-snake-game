d62f03c7a204bb239429c5bc50383636298835fb
DakshAgarwal2002 <dakshagarwalno1.gmail.com>
2023-01-11 16:53:30 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,168 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Snake from "./Snake";
+import Food from './Food';
+  
+const getRandomCoordinates=()=>
+{
+  let min=1;
+  let max=98;
+  let x=Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  let y=Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  return [x,y];
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState={
+  food:getRandomCoordinates(),
+  speed:200,
+  direction:'RIGHT',
+  snakeDots:[[0,0],[2,0]]
+}
+
+class App extends Component
+{
+  state=initialState;
+  componentDidMount()
+  {
+    setInterval(this.moveSnake,this.state.speed)
+    document.onkeydown=this.onKeyDown;
+  }
+
+  componentDidUpdate()
+  {
+    this.checkOutOfBorder();
+    this.checkIfCollapsed();
+    this.checkIfEat();
+  }
+
+  onKeyDown=(e)=>
+  {
+    e=e||window.event;
+    switch(e.keyCode)
+    {
+      case 87:
+        this.setState({direction:'UP'});
+        break;
+      case 83:
+        this.setState({direction:'DOWN'});
+        break;
+      case 65:
+        this.setState({direction:'LEFT'});
+        break;
+      case 68:
+        this.setState({direction:'RIGHT'});
+        break;
+    }
+  }
+
+  moveSnake=()=>
+  {
+    let dots=[...this.state.snakeDots];
+    let head=dots[dots.length-1];
+    switch(this.state.direction)
+    {
+      case 'RIGHT':
+        head=[head[0]+2,head[1]];
+        break;
+      case 'LEFT':
+        head=[head[0]-2,head[1]];
+        break;
+      case 'DOWN':
+        head=[head[0],head[1]+2];
+        break;
+      case 'UP':
+        head=[head[0],head[1]-2];
+        break;
+    }
+    dots.push(head);
+    dots.shift();
+    this.setState(
+      {
+        snakeDots:dots
+      }
+    )
+  }
+
+  checkOutOfBorder()
+  {
+    let head=this.state.snakeDots[this.state.snakeDots.length-1];
+    if(head[0]>=100 || head[1]>=100 || head[0]<0 || head[1]<0)
+    {
+      this.onGameOver();
+    }
+  }
+  checkIfCollapsed()
+  {
+    let snake=[...this.state.snakeDots];
+    let head=snake[snake.length-1];
+    snake.pop();
+    snake.forEach(dot=>{
+      if(head[0]==dot[0] && head[1]==dot[1])
+      {
+        this.onGameOver();
+      }
+    })
+  }
+
+  checkIfEat()
+  {
+    let head=this.state.snakeDots[this.state.snakeDots.length-1];
+    let food=this.state.food;
+    if(head[0]==food[0] && head[1]==food[1])
+    {
+      this.setState({
+        food:getRandomCoordinates()
+      })
+      this.enlargeSnake();
+      this.increaseSpeed();
+    }
+  }
+
+  enlargeSnake()
+  {
+    let newSnake=[...this.state.snakeDots];
+    newSnake.unshift([])
+    this.setState({
+      snakeDots:newSnake
+    })
+  }
+
+  increaseSpeed()
+  {
+    if(this.state.speed >10)
+    {
+      this.setState({
+        speed:this.state.speed-10
+      })
+    }
+  }
+
+  onGameOver=()=>
+  {
+    alert(`Game Over, Snake length is ${this.state.snakeDots.length}`)
+    this.setState(initialState)
+  }
+ 
+  render()
+  {
+    return (
+      <>
+      <h1 className='game-title'>THE CLASSIC SNAKE GAME</h1>
+      <div className='background-area'>
+        <div className="game-area">
+        {/* <div className="snake-dot" style={{top:0, left:0}}></div>
+        <div className="snake-dot" style={{top:0, left:'2%'}}></div>
+        <div className="snake-dot" style={{top:0, left:'4%'}}></div> */}
+        <Snake snakeDots={this.state.snakeDots}/>
+        <Food dot={this.state.food}/>
+      </div>
+      </div>
+      <footer>
+        <h2>Made with love by Daksh Agarwal</h2>
+      </footer>
+      </>
+      
+    );
+  }
 }
 
 export default App;
